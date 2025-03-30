a8788533e29a19ecd1c8966bb8de3a4d8482dcc4
Mohammad Adnan <65945728+adnandotdev@users.noreply.github.com>
2025-03-30 21:03:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+function manipulateArray(){
+	new Promise((resolve) =>{
+			setTimeout(() => {
+				resolve([1, 2, 3, 4, 5])
+			}, 3000)
+	})
+	.then((array) => {
+		return new Promise((resolve) => {
+			setTimeout(() => {
+				let evens = array.filter(num => num%2 === 0)
+				document.getElementById("output").textContent = JSON.stringify(evens)
+				resolve(evens)
+			}, 1000)
+		})
+	})
+	.then((evenNums) => {
+		return new Promise((resolve) => {
+			setTimeout(() => {
+				let multipliedArr = evenNums.map(num => num*2)
+				document.getElementById("output").textContent = JSON.stringify(multipliedArr)
+				resolve(multipliedArr)
+			}, 2000)
+		})
+	})
+}
+window.onload = manipulateArray()
